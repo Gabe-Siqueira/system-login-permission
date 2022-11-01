33cd56ff01d5f884e3ec88d3cd488f3e09d87012
Gabe-Siqueira <gabriel.snsilva2@gmail.com>
2022-11-01 02:50:56 -0300

    create controller api user

---
+++ app/Http/Controllers/Api/UserApiController.php
@@ -0,0 +1,180 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\LibraryController;
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class UserApiController extends Controller
+{
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public static function index()
+    {
+        try {
+            $user = new User();
+            $response = $user->get();
+
+            return LibraryController::responseApi($response);
+        } catch (Exception $e) {
+            LibraryController::recordError($e);
+            if ($e->getCode()) {
+                $code = $e->getCode();
+            }else {
+                $code = 500;
+            }
+            return response()->json(LibraryController::responseApi([],$e->getMessage(), $code, false));
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public static function store(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'id_profile' => 'required|integer',
+                'name' => 'required|max:255',
+                'email' => 'required|unique:users|max:255',
+                'password' => 'required|max:255'
+            ], [
+                'email.unique' => 'Email "'.$request->email.'" já está em uso.',
+            ], [
+                'id_profile'      => 'Perfil',
+                'name'      => 'Usuário',
+                'email'      => 'Email',
+                'password'      => 'Senha',
+            ]);
+
+            if ($validator->fails()) {
+                return LibraryController::responseApi($validator, $validator->getMessageBag(), true, false);
+            }
+
+            $user = new User();
+            $user->fill($request->all());
+            $user->password = bcrypt($request->password);
+            $user->save();
+            return LibraryController::responseApi($user);
+        } catch (Exception $e) {
+            LibraryController::recordError($e);
+            if ($e->getCode()) {
+                $code = $e->getCode();
+            }else {
+                $code = 500;
+            }
+            return response()->json(LibraryController::responseApi([],$e->getMessage(), $code, false));
+        }
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public static function show($id)
+    {
+        try {
+            $user = new User();
+            $user = $user::findOrFail($id);
+            $response = $user->where('id', $id)->get();
+            return LibraryController::responseApi($response);
+        } catch (Exception $e) {
+            LibraryController::recordError($e);
+            if ($e->getCode()) {
+                $code = $e->getCode();
+            }else {
+                $code = 500;
+            }
+            return response()->json(LibraryController::responseApi([],$e->getMessage(), $code, false));
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public static function update(Request $request, $id)
+    {
+        try {
+
+            if (is_null($request->password)) {
+                $request->request->add(['password' => $request->confirm_password]);
+            }
+
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|max:255',
+                'email' => 'required|max:255|unique:users,email,'.$id,
+                'password' => 'max:255',
+                'id_profile' => 'required'
+            ], [
+                'email.unique' => 'Email "'.$request->email.'" já está em uso.',
+            ], [
+                'name'      => 'Usuário',
+                'email'      => 'Email',
+                'password'      => 'Senha',
+                'id_profile' => 'Profile'
+            ]);
+
+            if ($validator->fails()) {
+                return LibraryController::responseApi($validator, $validator->getMessageBag(), true, false);
+            }
+
+            $user = new User();
+            $user = $user->findOrFail($id);
+            $user->fill($request->all());
+            LibraryController::logupdate($user);
+            $user->save();
+            return LibraryController::responseApi($user, 'ok', false, true);
+        } catch (Exception $e) {
+            LibraryController::recordError($e);
+            if ($e->getCode()) {
+                $code = $e->getCode();
+            }else {
+                $code = 500;
+            }
+            return response()->json(LibraryController::responseApi([],$e->getMessage(), $code, false));
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public static function destroy($id)
+    {
+        try {
+            $user = new User();
+            $user = $user->findOrFail($id);
+            $user->delete();
+            return LibraryController::responseApi($user, 'ok');
+        } catch (Exception $e) {
+            LibraryController::recordError($e);
+            if ($e->getCode()) {
+                $code = $e->getCode();
+            }else {
+                $code = 500;
+            }
+            return response()->json(LibraryController::responseApi([],$e->getMessage(), $code, false));
+        }
+
+    }
+}
