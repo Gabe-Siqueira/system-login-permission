cc8ed07656d75d6748bc62ac577f5dec68834c77
Gabe-Siqueira <gabriel.snsilva2@gmail.com>
2022-11-01 03:52:35 -0300

    create blade permission create

---
+++ resources/views/permission/create.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.authBase')
+
+@section('content')
+
+<style>
+    .bold
+    {
+        font-weight: bold;
+    }
+</style>
+
+<main class="permission-form">
+    <div class="container mt-3">
+        <div class="row justify-content-center">
+            <div class="container-fluid">
+                <!-- INCIO CARD CRIAR PERMISSION -->
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row ml-1">
+                                    <h2 class="bold">
+                                        Cadastrar Permissão
+                                    </h2>
+                                </div>
+                                <x-erro :message="$errors->all()"/>
+                                <x-sucesso message="{{ session('success') }}" />
+                                <form action="{{route('permission.store')}}" method="post">
+                                    @csrf
+                                    <div class="permission">
+                                        <div class="row ml-1">
+                                            <h4 class="bold">
+                                                Dados
+                                            </h4>
+                                        </div>
+                                        <hr style="margin-top: -5px;">
+                                        <div class="row">
+                                            <div class="col-sm-12 col-md-12 col-lg-12 col-xs-12">
+                                                <div class="form-permission">
+                                                    <b>Nome:</b>
+                                                    <input type="text" class="form-control" name="name" maxlength="255" required >
+                                                    <span class="error-validate"><p id="validate-name"></p></span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row ml-1">
+                                        <div class="form-group">
+                                            <div class="btn-group" role="group">
+                                                <a class="btn btn-primary btn-sm" href="{{ route('permission.index') }}" role="button">
+                                                    <i class="fa fa-arrow-circle-o-left" aria-hidden="true"></i> Voltar
+                                                </a>
+                                            </div>
+                                            <div class="btn-group" role="group">
+                                                <button class="btn btn-success btn-sm" type="submit">
+                                                    <i class="fa fa-check-circle-o" aria-hidden="true"></i> Cadastrar
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- FIM CARD CRIAR PERMISSION -->
+            </div>
+        </div>
+    </div>
+</main>
+
+@endsection
