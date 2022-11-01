eea2b12ae99d6f962f6db7868322ce69955f9fd7
Gabe-Siqueira <gabriel.snsilva2@gmail.com>
2022-11-01 02:55:15 -0300

    update routes web

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Admin\UserAdminController;
+use App\Http\Controllers\Admin\MenuAdminController;
+use App\Http\Controllers\Admin\PermissionAdminController;
 use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
@@ -31,6 +34,30 @@ Route::get('/sessionexpire', [HomeController::class, 'sessionexpire'])->name('ho
 
     Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-    // Route::resource('/book', BookController::class);
+    // Route::resource('/user', UserController::class);
+
+    // UserAdminController
+    Route::get('/user',  [UserAdminController::class, 'index'])->name('user.index');
+    Route::get('/user/create',  [UserAdminController::class, 'create'])->name('user.create');
+    Route::post('/user',  [UserAdminController::class, 'store'])->name('user.store');
+    Route::get('/user/{id}',  [UserAdminController::class, 'edit'])->name('user.edit');
+    Route::put('/user/{id}',  [UserAdminController::class, 'update'])->name('user.update');
+    Route::delete('/user/{id}',  [UserAdminController::class, 'destroy'])->name('user.destroy');
+
+    // MenuAdminController
+    Route::get('/menu',  [MenuAdminController::class, 'index'])->name('menu.index');
+    Route::get('/menu/create',  [MenuAdminController::class, 'create'])->name('menu.create');
+    Route::post('/menu',  [MenuAdminController::class, 'store'])->name('menu.store');
+    Route::get('/menu/{id}',  [MenuAdminController::class, 'edit'])->name('menu.edit');
+    Route::put('/menu/{id}',  [MenuAdminController::class, 'update'])->name('menu.update');
+    Route::delete('/menu/{id}',  [MenuAdminController::class, 'destroy'])->name('menu.destroy');
+
+    // PermissionAdminController
+    Route::get('/permission',  [PermissionAdminController::class, 'index'])->name('permission.index');
+    Route::get('/permission/create',  [PermissionAdminController::class, 'create'])->name('permission.create');
+    Route::post('/permission',  [PermissionAdminController::class, 'store'])->name('permission.store');
+    Route::get('/permission/{id}',  [PermissionAdminController::class, 'edit'])->name('permission.edit');
+    Route::put('/permission/{id}',  [PermissionAdminController::class, 'update'])->name('permission.update');
+    Route::delete('/permission/{id}',  [PermissionAdminController::class, 'destroy'])->name('permission.destroy');
 
 // });
