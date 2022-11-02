e5d5ffb81e2b1687b8f49140984b72aab10b7571
Gabe-Siqueira <gabriel.snsilva2@gmail.com>
2022-11-01 23:53:08 -0300

    create blade index file

---
+++ resources/views/file/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.authBase')
+
+@section('content')
+
+    <main class="home-form">
+        <div class="cotainer">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="jumbotron">
+                        <h1 class="display-4">Arquivos</h1>
+                        <p class="lead">Tela Arquivo</p>
+                        <hr class="my-4">
+                        <p class="lead">
+                            <a class="btn btn-info btn-sm mb-3" href="{{ route('home') }}">Voltar</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </main>
+
+@endsection
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js" type="text/javascript"></script>
