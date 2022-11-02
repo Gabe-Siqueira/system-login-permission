50e9ee1336efdea23f7989e3732803a9824f174b
Gabe-Siqueira <gabriel.snsilva2@gmail.com>
2022-11-01 23:49:42 -0300

    create controller course -> add function index

---
+++ app/Http/Controllers/Admin/CourseAdminController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\LibraryController;
+use Exception;
+use Illuminate\Http\Request;
+
+class CourseAdminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        try {
+            return view('course.index');
+        } catch (Exception $e) {
+            throw $e;
+            return LibraryController::responseApi(["title" => __('messages.titleLoadPageError'), "message" => __('messages.defaultMessage')], "", 500, false);
+        }
+    }
+}
