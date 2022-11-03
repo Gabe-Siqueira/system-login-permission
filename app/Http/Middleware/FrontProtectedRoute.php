2627468e53d8febc116d3d1d9210fe8124e0e9d3
Gabe-Siqueira <gabriel.snsilva2@gmail.com>
2022-11-03 00:10:30 -0300

    create Middleware FrontProtectedRoute

---
+++ app/Http/Middleware/FrontProtectedRoute.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class FrontProtectedRoute
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $user = Auth::user();
+
+        if (!$user) {
+            return response(['Forbidden'], 403);
+        }
+
+        $route = request()->route();
+
+        if ($route->uri == 'home') {
+            return $next($request);
+        }else{
+            foreach ($user->menu as $key => $value) {
+                $str = $value->link;
+                if (strpos($str, $route->uri) !== false) {
+                    return $next($request);
+                }
+            }
+        }
+
+        return response(['Forbidden'], 403);
+
+        return $next($request);
+    }
+}
